451db8cf130f97bed55cdc2983a7c12fbc8942cc
EHeathcote <emh4226@gmail.com>
2024-03-25 15:48:45 -0400

    added js to save selected seats, movie index, and movie price to local storage

--- Movie Seat Booking/script.js
+++ Movie Seat Booking/script.js
@@ -6,23 +6,36 @@ const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
 let ticketPrice = parseInt(movieSelect.value);
+//save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
 
-//update total and count
+//update total and count and save seats to local storage
 function updateSelectedCount() {
   // NodeList of selected seats
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
   const selectedSeatsCount = selectedSeats.length;
-  count.innerText = selectedSeatsCount;
-  console.log(count.innerText);
 
+  // copy selected seats into an array and map to get array of seat indexes
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+  //save selected seats index array
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+  //chagne inner text of count and total to match select
+  count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 }
+
 //movie select event
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+  //save movie selected and price to local storage
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
-//event listener for click on a seat to toggle color and update count and total text
+
+//event listener for click on a seat
 container.addEventListener("click", (e) => {
   if (
     e.target.classList.contains("seat") &&
